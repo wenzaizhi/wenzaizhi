83fecbc292851c168b574c78866f4632bf1405a1
exzzx <g359340318@gmail.com>
2026-01-26 18:34:30 +0800

    reorder chapters

--- 问灾志章节 02卷/rename_chapter_prefix.sh
+++ 问灾志章节 02卷/rename_chapter_prefix.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-for i in {65..23}; do
+for i in {71..63}; do
     prefix=$(printf "%03d章" "$i")
-    next=$(printf "%03d章" "$((i+1))")
+    next=$(printf "%03d章" "$((i-30))")
     for f in "$prefix"*; do
         [ -e "$f" ] || continue
         git mv "$f" "$next${f#$prefix}"
